d1e88b530a3a232d61ffc398d894a93da731c031
Denis Malykh <mrdekk@yandex-team.ru>
2016-05-04 10:30:26 +0500

    add: schemes for build with carthage

---
+++ DataKernel-iOS/DataKernel-iOS.h
@@ -0,0 +1,19 @@
+//
+//  DataKernel-iOS.h
+//  DataKernel-iOS
+//
+//  Created by Denis Malykh on 04.05.16.
+//  Copyright © 2016 mrdekk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DataKernel-iOS.
+FOUNDATION_EXPORT double DataKernel_iOSVersionNumber;
+
+//! Project version string for DataKernel-iOS.
+FOUNDATION_EXPORT const unsigned char DataKernel_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataKernel_iOS/PublicHeader.h>
+
+
